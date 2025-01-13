a682192e797b93363e7f0006907ffe8541406380
bopj <bopj@softex.cin.ufpe.br>
2025-01-13 09:50:31 -0300

    CHORE: project setup for building

---
+++ fsm_tester/__init__.py
@@ -0,0 +1,6 @@
+from fsm_tester.fsm_tester import FSMTester
+from fsm_tester.entities.fsm_protocol import FSMProtocol
+from fsm_tester.typing import DIALECTS
+
+
+__all__ = ['FSMTester', 'FSMProtocol', 'DIALECTS']
